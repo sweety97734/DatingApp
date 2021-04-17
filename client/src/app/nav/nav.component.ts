55ef4f08db9306bf6a71e56bb127ef9a45701102
Sweety <sweety.prasad@ascendlearning.com>
2021-04-17 22:59:32 +0530

    End of section 7

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -22,9 +22,6 @@ export class NavComponent implements OnInit {
   login(){
     this.accountService.login(this.model).subscribe(response => {
       this.router.navigateByUrl('/members');
-    }, error =>{
-      console.log(error); //error handling
-      this.toastr.error(error.error);
     })
   }
 
