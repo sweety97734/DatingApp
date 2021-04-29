553b42fce4920bb475d6953a71aa931e2cf7d319
Sweety <sweety.prasad@ascendlearning.com>
2021-04-29 14:02:35 +0530

    End of section 14

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,15 +1,14 @@
 using System;
 using System.Collections.Generic;
 using API.Extensions;
-
 namespace API.Entities
 {
     public class AppUser
     {
         public int Id { get; set; }
         public string UserName { get; set; }
-        public byte[] PasswordHash { get; set; } //Hashing performs a one-way transformation on a password, turning the password into another String, called the hashed password.
-        public byte[] PasswordSalt { get; set; } //Salting is simply the addition of a unique, random string of characters known only to the site to each password before it is hashed, typically this “salt” is placed in front of each password
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; } = DateTime.Now;
@@ -21,10 +20,8 @@ namespace API.Entities
         public string City { get; set; }
         public string Country { get; set; }
         public ICollection<Photo> Photos { get; set; }
+        public ICollection<UserLike> LikedByUsers { get; set; }
+        public ICollection<UserLike> LikedUsers { get; set; }
 
-        //public int GetAge()
-       // {
-           // return DateOfBirth.CalculateAge();
-        //}
     }
 }
--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -8,7 +8,6 @@ import { PaginatedResult } from '../_models/pagination';
 import { UserParams } from '../_models/userParams';
 import { AccountService } from './account.service';
 import { User } from '../_models/user';
-
 @Injectable({
   providedIn: 'root'
 })
@@ -18,58 +17,47 @@ export class MembersService {
   memberCache = new Map();
   user: User;
   userParams: UserParams;
-
   constructor(private http: HttpClient, private accountService: AccountService) {
     this.accountService.currentUser$.pipe(take(1)).subscribe(user => {
       this.user = user;
       this.userParams = new UserParams(user);
     })
   }
-
   getUserParams() {
     return this.userParams;
   }
-
   setUserParams(params: UserParams) {
     this.userParams = params;
   }
-
   resetUserParams() {
     this.userParams = new UserParams(this.user);
     return this.userParams;
   }
-
   getMembers(userParams: UserParams) {
     var response = this.memberCache.get(Object.values(userParams).join('-'));
     if (response) {
       return of(response);
     }
-
     let params = this.getPaginationHeaders(userParams.pageNumber, userParams.pageSize);
-
     params = params.append('minAge', userParams.minAge.toString());
     params = params.append('maxAge', userParams.maxAge.toString());
     params = params.append('gender', userParams.gender);
     params = params.append('orderBy', userParams.orderBy);
-
     return this.getPaginatedResult<Member[]>(this.baseUrl + 'users', params)
       .pipe(map(response => {
         this.memberCache.set(Object.values(userParams).join('-'), response);
         return response;
       }))
   }
-
   getMember(username: string) {
     const member = [...this.memberCache.values()]
-      .reduce((arr, elem) => arr.concat(elem.result), []) // console.log(member)
+      .reduce((arr, elem) => arr.concat(elem.result), [])
       .find((member: Member) => member.username === username);
-
     if (member) {
       return of(member);
     }
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
-
   updateMember(member: Member) {
     return this.http.put(this.baseUrl + 'users', member).pipe(
       map(() => {
@@ -85,6 +73,16 @@ export class MembersService {
     return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
   }
 
+  addLike(username: string) {
+    return this.http.post(this.baseUrl + 'likes/' + username, {})
+  }
+
+  getLikes(predicate: string, pageNumber, pageSize) {
+    let params = this.getPaginationHeaders(pageNumber, pageSize);
+    params = params.append('predicate', predicate);
+    return this.getPaginatedResult<Partial<Member[]>>(this.baseUrl + 'likes', params);
+  }
+
   private getPaginatedResult<T>(url, params) {
     const paginatedResult: PaginatedResult<T> = new PaginatedResult<T>();
     return this.http.get<T>(url, { observe: 'response', params }).pipe(
@@ -97,13 +95,10 @@ export class MembersService {
       })
     );
   }
-
   private getPaginationHeaders(pageNumber: number, pageSize: number) {
     let params = new HttpParams();
-
     params = params.append('pageNumber', pageNumber.toString());
     params = params.append('pageSize', pageSize.toString());
-
     return params;
   }
 }
